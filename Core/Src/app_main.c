0202409c2aac79216ae816f969f29032f0527a68
MarioGavran <mario.gavran@student.um.si>
2021-12-10 17:08:46 +0100

    update & clean

--- Core/Src/app_main.c
+++ Core/Src/app_main.c
@@ -34,7 +34,7 @@ void app_main_init()
 	LCD_SetDirection(HORIZONTAL_RIGHT);
 
 	//==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~==~~
-	// FERI logo
+	// ~=~=~ FERI logo ~=~=~
 	LCD_ClearScreen(0xFFFFU);
 	LCD_SetWindow(240, 184, 240 + FERI_LOGO_IMG_W -1, 184 + FERI_LOGO_IMG_H -1);
 	for(l = 0; l < FERI_LOGO_IMG_SIZE; l+=2)
@@ -71,7 +71,7 @@ void app_main_init()
 			FSMC_WR_DAT(0xFFFFU);
 		}
 	}
-	// Add FERI logo at the end
+	// ~=~=~ Add FERI logo at the end ~=~=~
 	LCD_SetWindow(240, 140, 240 + FERI_LOGO_IMG_W -1, 140 + FERI_LOGO_IMG_H -1);
 	for(l = 0; l < FERI_LOGO_IMG_SIZE; l+=2)
 	{
@@ -88,7 +88,7 @@ void app_main_init()
 	LCD_DrawLine(10, 10, 10, 470, 0x07E0U,4);		// left
 	LCD_DrawLine(790, 10, 790, 470, 0x001FU,4);		// right
 	LCD_DrawLine(10, 470, 790, 470, 0xF800U,4);		// down
-*/
+	*/
 
 
 	ov7670_init();
@@ -99,18 +99,13 @@ void app_main_init()
 void app_main_loop()
 {
 	uint8_t buff[10] = {0};
-	//uint8_t i2cDatToSnd[5] = {0};
-	//uint8_t i2cDatToRec[5] = {0};
 	uint32_t milis = 0;
-	uint32_t milis2 = 0;
 	int j,z = 0;
 	int k = -2;
-	int kk = 0;
 
 	LCD_SetDirection(VERTICAL_UP);
 	LCD_SetWindow(20, 20, 340-1, 260-1); // 320 x 240
 	tflite_micro_setup();
-
 
 
 	while(1)
@@ -135,43 +130,15 @@ void app_main_loop()
 		uart_rx_process();
 
 		LCD_SetWindow(20 , 280, 20+96-1, 280+96-1); // 320 x 240
-/*
- *
-		//*********************************************************************************************************************
-		//*********************************************************************************************************************
-		for(z = 0; z < 96*96; z++)
-		{
-			char gray = ((
-					((g_person_image_data1[z] & 0xF8U) >> 11) +	//red
-					((g_person_image_data1[z] & 0x7CU) << 6) +	//green
-					((g_person_image_data1[z] & 0x1FU))	//blue
-					) / 3) & 0xFFU;
-
-			uint16_t graysc = (
-					((gray << 11) & 0xF800U) |
-					((gray << 6)  & 0x07C0U) |
-					((gray << 0)  & 0x001FU));
-
-			g_cam_gray_frame[z] = gray;
-
-			FSMC_WR_DAT(graysc);
-		}
- */
 
 		//*********************************************************************************************************************
-		//*********************************************************************************************************************
-
-		//*********************************************************************************************************************
-		//*********************************************************************************************************************
-		/**/
-
 		for(z = 0; z < 96*96*2; z+=2)
 		{
 			// Calculate gray-scale from RGB data
 			char gray = (
-					(((g_person_image_data1[z] & 0xF8U) >> 3) +	//red
+					(((g_person_image_data1[z] & 0xF8U) >> 3) +													//red
 					(((g_person_image_data1[z] & 0x07U) << 2) | ((g_person_image_data1[z+1] & 0xC0U) >> 6)) +	//green
-					(((g_person_image_data1[z+1] & 0x1FU)))	//blue
+					(((g_person_image_data1[z+1] & 0x1FU)))														//blue
 					) / 3);
 
 			// Assemble gray-scale into RGB565 format
@@ -180,63 +147,32 @@ void app_main_loop()
 					((gray << 6)  & 0x07C0U) |
 					((gray << 0)  & 0x001FU));
 
-			g_cam_gray_frame[kk] = gray << 3;	// scale up
+			g_cam_gray_frame[k] = gray << 3;	// scale up
 
-			kk++;
-			if(kk >= OV7670_GRAY_SIZE)
-				kk = 0;
+			k++;
+			if(k >= OV7670_GRAY_SIZE)
+				k = 0;
 
 			FSMC_WR_DAT(graysc);
 		}
-
-			//*********************************************************************************************************************
-			//*********************************************************************************************************************
-
-
-	/*
 		//*********************************************************************************************************************
-	 	//*********************************************************************************************************************
-		for(z = 0; z < 96*96; z++)
-		{
-			//char gray = ((
-			//		((g_person_image_data1[z] & 0xE0U) >> 5) +	//red
-			//		((g_person_image_data1[z] & 0x1CU) >> 2) +	//green
-			//		((g_person_image_data1[z] & 0x03U) >> 0)	//blue
-			//		) );
-
-			uint16_t graysc = (
-					((g_person_image_data1[z] << 11) & 0xF800U) |
-					((g_person_image_data1[z] << 6)  & 0x07C0U) |
-					((g_person_image_data1[z] << 0)  & 0x001FU));
-
-			g_cam_gray_frame[z] = g_person_image_data1[z];
-			FSMC_WR_DAT(graysc);
-		}
-		//*********************************************************************************************************************
-		//*********************************************************************************************************************
-*/
-
-
 
 		LCD_SetWindow(20, 20, 340-1, 260-1); // 320 x 240
 		for(j = 0; j <= OV7670_FRAME_SIZE_QVGA - 2; j+=2)
 		{
-			//
+			/*
 			// Black & White Video
-			//uint16_t gray =
-			//		(((g_cam_buff[j] & 0xF8U) >> 3) +	//red
-			//				(((g_cam_buff[j] & 0x07U) << 3) | ((g_cam_buff[j+1] & 0xC0U) >> 6)) +	//green
-			//				((g_cam_buff[j+1] & 0x1FU))	//blue
-			//		) / 3;
-//
-			//uint16_t graysc =
-			//		(((gray << 11) & 0xF800U) |
-			//		((gray << 6) & 0x07C0U) |
-			//		(gray & 0x001FU));
-			// FSMC_WR_DAT(graysc);
+			uint16_t gray =(((g_cam_buff[j] & 0xF8U) >> 3) +										//red
+							(((g_cam_buff[j] & 0x07U) << 3) | ((g_cam_buff[j+1] & 0xC0U) >> 6)) +	//green
+							((g_cam_buff[j+1] & 0x1FU))												//blue
+					) / 3;
 
-
-			// RRRRR GGGGGG BBBBB
+			uint16_t graysc =
+					(((gray << 11) & 0xF800U) |
+					((gray << 6) & 0x07C0U) |
+					(gray & 0x001FU));
+			FSMC_WR_DAT(graysc);
+			*/
 			FSMC_WR_DAT(((g_cam_buff[j] << 8) & 0xFF00U) | (g_cam_buff[j+1] & 0x00FFU));
 		}
 
@@ -249,21 +185,11 @@ void app_main_loop()
 			}
 			else
 			{// every second
-				sprintf(buff, "0x\r\n");
+				sprintf(buff, "\r\nHello");
 				uart_write(buff);
 			}
 		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
 
 
